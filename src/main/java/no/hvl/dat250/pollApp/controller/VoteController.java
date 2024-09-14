ee45bddb3ee09aca7fe8104aa486f1490103688c
Ivhene <ivarh.nesheim@gmail.com>
2024-09-15 01:14:38 +0200

    fix: change to specify CrossOrigin

--- src/main/java/no/hvl/dat250/pollApp/controller/VoteController.java
+++ src/main/java/no/hvl/dat250/pollApp/controller/VoteController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("polls/{pollId}/voteoptions/{voteoptionId}/votes")
 public class VoteController {
 
